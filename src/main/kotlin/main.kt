7bd51cf063b9755b9b15e9cafe2987efaf9fe5ae
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2022-06-02 10:10:49 -0300

    feat: add base `play` command

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -7,7 +7,11 @@
 
 package gragas
 
+import dev.kord.common.entity.Snowflake
 import dev.kord.core.Kord
+import dev.kord.core.behavior.interaction.response.respond
+import dev.kord.core.event.interaction.GuildChatInputCommandInteractionCreateEvent
+import dev.kord.rest.builder.interaction.string
 import java.lang.System.getenv
 import java.lang.System.setProperty
 import java.util.concurrent.Executors
@@ -15,6 +19,10 @@ import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.DEBUG_PROPERTY_NAME
 import kotlinx.coroutines.DEBUG_PROPERTY_VALUE_ON
 import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.filterIsInstance
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.runBlocking
 import mu.KotlinLogging
 
@@ -34,8 +42,34 @@ suspend fun startBot() {
   log.info { "Starting bot..." }
 
   val token = getenv("DISCORD_TOKEN") ?: error("Can not find Bot token.")
-  val kord = Kord(token) {
-  }
 
+  val kord = Kord(token)
+  kord.playCommand()
   kord.login()
+}
+
+suspend fun Kord.playCommand() {
+  events
+    .filterIsInstance<GuildChatInputCommandInteractionCreateEvent>()
+    .filter { it.interaction.command.rootName == "play" }
+    .map { event ->
+      val command = event.interaction.command
+
+      val url = command.strings["url"] ?: error("unreachable")
+
+      event.interaction.deferPublicResponse().respond {
+        content = "You have played the song $url"
+      }
+    }
+    .launchIn(this)
+
+  createGuildChatInputCommand(
+    guildId = Snowflake(731161106797559841L),
+    name = "play",
+    description = "Plays a specified song in your channel",
+  ) {
+    string("url", "The song URL") {
+      required = true
+    }
+  }
 }
